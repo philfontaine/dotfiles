253d26c50033c0ce8a7ab83e33e3526a0a06070d
pfontaine <pfontaine@tecnar.com>
2026-04-08 15:12:46 -0400

    feat: add powershell profile

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,6 @@
+# Add . ~/.config/powershell/profile.ps1 to $PROFILE
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Set-Alias -Name cl -Value claude
+Set-Alias -Name lg -Value lazygit
+Set-Alias -Name wt -Value wezterm
