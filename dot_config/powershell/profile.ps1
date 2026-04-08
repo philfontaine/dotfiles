e2da330a036b227d2e159f9023c724f09ad4e6e8
pfontaine <pfontaine@tecnar.com>
2026-04-08 15:16:06 -0400

    feat: add cm for chezmoi

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -4,3 +4,4 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Set-Alias -Name cl -Value claude
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name wt -Value wezterm
+Set-Alias -Name cm -Value chezmoi
