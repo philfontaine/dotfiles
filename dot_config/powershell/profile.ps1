1a361c1f449298d112b894514d0978c5922d353c
pfontaine <pfontaine@tecnar.com>
2026-05-20 13:24:06 -0400

    chore: reorder/format

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,8 +1,13 @@
 # Add . ~/.config/powershell/profile.ps1 to $PROFILE
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Set-Alias -Name cl -Value claude
 
+Set-Alias -Name cl -Value claude
+Set-Alias -Name lg -Value lazygit
+Set-Alias -Name wt -Value wezterm
+Set-Alias -Name cm -Value chezmoi
+
+# Claude Code OpenRouter
 function clor {
     $claudeEnv = [System.Environment]::GetEnvironmentVariables()
     $claudeEnv["ANTHROPIC_BASE_URL"] = "https://openrouter.ai/api"
@@ -13,6 +18,3 @@ function clor {
     $claudeEnv["ANTHROPIC_DEFAULT_HAIKU_MODEL"] = "deepseek/deepseek-v4-flash"
     Start-Process -FilePath claude -Environment $claudeEnv -NoNewWindow -Wait -ArgumentList $args
 }
-Set-Alias -Name lg -Value lazygit
-Set-Alias -Name wt -Value wezterm
-Set-Alias -Name cm -Value chezmoi
