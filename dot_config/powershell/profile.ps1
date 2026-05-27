9bcac319bcf0df551fbbaad8df7cad4004946092
pfontaine <pfontaine@tecnar.com>
2026-05-27 15:27:34 -0400

    feat: use deepseek only
    Other models cost too much

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -13,8 +13,8 @@ function clor {
     $claudeEnv["ANTHROPIC_BASE_URL"] = "https://openrouter.ai/api"
     $claudeEnv["ANTHROPIC_AUTH_TOKEN"] = $claudeEnv["OPENROUTER_API_KEY"]
     $claudeEnv["ANTHROPIC_API_KEY"] = ""
-    $claudeEnv["ANTHROPIC_DEFAULT_OPUS_MODEL"] = "openai/gpt-5.5"
-    $claudeEnv["ANTHROPIC_DEFAULT_SONNET_MODEL"] = "google/gemini-3.5-flash"
+    $claudeEnv["ANTHROPIC_DEFAULT_OPUS_MODEL"] = "deepseek/deepseek-v4-pro"
+    $claudeEnv["ANTHROPIC_DEFAULT_SONNET_MODEL"] = "deepseek/deepseek-v4-flash"
     $claudeEnv["ANTHROPIC_DEFAULT_HAIKU_MODEL"] = "deepseek/deepseek-v4-flash"
     Start-Process -FilePath claude -Environment $claudeEnv -NoNewWindow -Wait -ArgumentList $args
 }
